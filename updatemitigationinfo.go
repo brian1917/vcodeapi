bb622db2df664b68b1e1418918cfe633d11fe021
Brian Pitta <brianpitta@gmail.com>
2017-10-04 21:38:02 -0400

    added updatemitigationinfo api

---
+++ updatemitigationinfo.go
@@ -0,0 +1,47 @@
+package vcodeapi
+
+import (
+	"net/http"
+	"io/ioutil"
+	"errors"
+	"net/url"
+	"strings"
+	"log"
+)
+
+func updateMitigationInfo(username, password, buildID, action, comment, flawList string) ([]byte, error) {
+	var errorMsg error = nil
+
+	client := http.Client{}
+
+	form := url.Values{}
+	form.Add("build_id", buildID)
+	form.Add("action", action)
+	form.Add("comment", comment)
+	form.Add("flaw_id_list", flawList)
+
+	req, err := http.NewRequest("POST", "https://analysiscenter.veracode.com/api/updatemitigationinfo.do",
+		strings.NewReader(form.Encode()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.SetBasicAuth(username, password)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if resp.Status != "200 OK" {
+		errorMsg = errors.New("updatemitigationinfo.do call error: " + resp.Status)
+	}
+
+	return data, errorMsg
+}
---
+++ updatemitigationinfoparser.go
@@ -0,0 +1,35 @@
+package vcodeapi
+
+import (
+	"log"
+	"encoding/xml"
+	"bytes"
+	"errors"
+)
+
+func ParseUpdateMitigation(username, password, buildID, action, comment, flawList string) (error) {
+	var errMsg error = nil
+
+	updateMitigationApi, err := updateMitigationInfo(username, password, buildID, action, comment, flawList)
+	if err!= nil{
+		log.Fatal(err)
+	}
+	decoder := xml.NewDecoder(bytes.NewReader(updateMitigationApi))
+	for {
+		// Read tokens from the XML document in a stream.
+		t, _ := decoder.Token()
+
+		if t == nil {
+			break
+		}
+		// Inspect the type of the token just read
+		switch se := t.(type) {
+		case xml.StartElement:
+			// Read StartElement and check for flaw
+			if se.Name.Local == "error" {
+				errMsg = errors.New("api for UpdateMitigationInfo returned with an error element")
+			}
+		}
+	}
+	return errMsg
+}
